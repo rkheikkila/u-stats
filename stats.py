f6eb39dc95326d995256140b253cd2b681731daa
rkheikkila <rkheikkila@gmail.com>
2016-05-20 14:11:19 +0300

    Enabled waitress

--- stats.py
+++ stats.py
@@ -343,7 +343,7 @@ def refresh():
           
 if __name__ == "__main__":
     s = RedditStats()
-    app.run(debug=True)
-    #port = int(os.environ.get('PORT', 5000))
-    #serve(app, host="0.0.0.0", port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    serve(app, host="0.0.0.0", port=port)
         
